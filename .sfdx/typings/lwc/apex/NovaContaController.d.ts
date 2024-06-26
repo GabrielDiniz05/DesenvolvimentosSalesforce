6e691abd8d45a8444dfbe2b6f83670aee11a0c74
Gabriel de Assis Lopes Diniz <83990190+GabrielDiniz05@users.noreply.github.com>
2024-06-26 11:39:14 -0300

    Delete .sfdx directory

--- .sfdx/typings/lwc/apex/NovaContaController.d.ts
+++
@@ -1,9 +0,0 @@
-declare module "@salesforce/apex/NovaContaController.getRTPessoaJuridica" {
-  export default function getRTPessoaJuridica(): Promise<any>;
-}
-declare module "@salesforce/apex/NovaContaController.getRTContaPessoal" {
-  export default function getRTContaPessoal(): Promise<any>;
-}
-declare module "@salesforce/apex/NovaContaController.callFastTrack" {
-  export default function callFastTrack(param: {acc: any}): Promise<any>;
-}
--- .sfdx/typings/lwc/apex/PdfPedidoController.d.ts
+++
@@ -1,9 +0,0 @@
-declare module "@salesforce/apex/PdfPedidoController.buscaOrder" {
-  export default function buscaOrder(param: {recordId: any}): Promise<any>;
-}
-declare module "@salesforce/apex/PdfPedidoController.buscaOrderItem" {
-  export default function buscaOrderItem(param: {recordId: any}): Promise<any>;
-}
-declare module "@salesforce/apex/PdfPedidoController.buscaUser" {
-  export default function buscaUser(param: {recordId: any}): Promise<any>;
-}

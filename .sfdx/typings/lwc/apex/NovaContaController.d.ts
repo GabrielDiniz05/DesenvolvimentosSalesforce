618434b5c1425a22edc343944153566ea97175b6
Gabriel Diniz <gabriel.assisdiniz@gmail.com>
2024-06-26 11:37:32 -0300

    Ajustes

---
+++ .sfdx/typings/lwc/apex/NovaContaController.d.ts
@@ -0,0 +1,9 @@
+declare module "@salesforce/apex/NovaContaController.getRTPessoaJuridica" {
+  export default function getRTPessoaJuridica(): Promise<any>;
+}
+declare module "@salesforce/apex/NovaContaController.getRTContaPessoal" {
+  export default function getRTContaPessoal(): Promise<any>;
+}
+declare module "@salesforce/apex/NovaContaController.callFastTrack" {
+  export default function callFastTrack(param: {acc: any}): Promise<any>;
+}
---
+++ .sfdx/typings/lwc/apex/PdfPedidoController.d.ts
@@ -0,0 +1,9 @@
+declare module "@salesforce/apex/PdfPedidoController.buscaOrder" {
+  export default function buscaOrder(param: {recordId: any}): Promise<any>;
+}
+declare module "@salesforce/apex/PdfPedidoController.buscaOrderItem" {
+  export default function buscaOrderItem(param: {recordId: any}): Promise<any>;
+}
+declare module "@salesforce/apex/PdfPedidoController.buscaUser" {
+  export default function buscaUser(param: {recordId: any}): Promise<any>;
+}
